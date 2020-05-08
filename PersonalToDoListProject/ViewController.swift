45443dc0568fa92f145d4e7f715593a80563ae18
Sean Duffy <seanodubhthaigh@gmail.com>
2020-05-08 15:35:57 +0100

    Initial Commit

---
+++ PersonalToDoListProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalToDoListProject
+//
+//  Created by Seán Duffy on 08/05/2020.
+//  Copyright © 2020 Seán Duffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
